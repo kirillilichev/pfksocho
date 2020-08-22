91fb621bc294bd6ebf4dc6e40c98ef2c6f6344bf
Kirill <kir-ilich@mail.ru>
2020-08-22 11:44:45 +0300

    Initial commit

---
+++ pfksochi/pfksochi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pfksochi
+//
+//  Created by  kirill on 22.08.2020.
+//  Copyright © 2020  kirill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
